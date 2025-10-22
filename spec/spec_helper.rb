452e9edce29b895c6368c1cfe4ce3f2195b84ae5
Keith Pitt <git@keithpitt.email>
2025-10-22 13:40:53 +0800

    Don't persit examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-require "bundler/setup"
-require "unmagic-enum"
+require 'bundler/setup'
+require 'unmagic-enum'
 
 # Conditionally require ActiveRecord for testing integration
 begin
-  require "active_record"
-  require "active_support/all"
+  require 'active_record'
+  require 'active_support/all'
 rescue LoadError
-  puts "⚠️  ActiveRecord not available - skipping ActiveRecord integration tests"
-  require "active_support"
+  puts '⚠️  ActiveRecord not available - skipping ActiveRecord integration tests'
+  require 'active_support'
 end
 
 RSpec.configure do |config|
@@ -23,19 +23,14 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = "spec/examples.txt"
   config.disable_monkey_patching!
   config.warnings = true
 
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
-  end
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   config.order = :random
   Kernel.srand config.seed
 
   # Skip ActiveRecord tests if ActiveRecord is not available
-  unless defined?(ActiveRecord)
-    config.filter_run_excluding :activerecord
-  end
+  config.filter_run_excluding :activerecord unless defined?(ActiveRecord)
 end
